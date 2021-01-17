e26ab10ce5c0e5a8096e84d5702eeba67d3b15c4
Josué Girón <benjamingiron1@gmail.com>
2021-01-17 02:21:32 -0600

    Agregar funciones de loggin para el consumo de los submódulos

---
+++ functions.go
@@ -0,0 +1,36 @@
+package consumer
+
+import (
+	"log"
+	"time"
+)
+
+// DefaultTimeout request doc ...
+var DefaultTimeout = 25 * time.Second
+
+// PrintError doc...
+var PrintError func(...interface{}) = log.Print
+
+// Print doc ...
+var Print func(string, ...interface{}) = log.Printf
+
+// Fatal doc ...
+var Fatal func(...interface{}) = log.Fatal
+
+// LogResponse doc ...
+func LogResponse(buff []byte) {
+	if len(buff) > 2000 {
+		Print("RESPONSE: ", string(buff[:1000]), " ••• SKIPED ••• ", string(buff[len(buff)-1000:]))
+	} else {
+		Print("RESPONSE: ", string(buff))
+	}
+}
+
+// LogRequest doc ...
+func LogRequest(buff []byte) {
+	if len(buff) > 2000 {
+		Print("REQUEST: ", string(buff[:1000]), " ••• SKIPED ••• ", string(buff[len(buff)-1000:]))
+	} else {
+		Print("REQUEST: ", string(buff))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jgolang/consumer
+
+go 1.15
